8909a6e35dbd46177bebee8f6709b180f651ea51
Manish Goel <manish.dce@gmail.com>
2023-01-03 11:46:53 +0530

    Initial setup

---
+++ setup.bash
@@ -0,0 +1,59 @@
+#!/bin/bash
+pushd . > /dev/null
+SCRIPTPATH="${BASH_SOURCE[0]}";
+while([ -h "${SCRIPTPATH}" ]); do
+	cd "`dirname "${SCRIPTPATH}"`"
+	SCRIPTPATH="$(readlink "`basename "${SCRIPTPATH}"`")";
+done
+cd "`dirname "${SCRIPTPATH}"`" > /dev/null
+SCRIPTPATH="`pwd`";
+popd  > /dev/null
+
+ln -s $SCRIPTPATH/dotfiles/.bashrc ~/.
+ln -s $SCRIPTPATH/dotfiles/.screenrc ~/.
+touch ~/.manishbashrc
+echo "Add custom setting to ~/.manishbashrc"
+
+
+sudo apt-get update -y
+
+# git installation
+sudo apt install git -y
+# install github cli
+sudo apt install gh -y
+gh auth login    # login, follow prompts
+
+# vim setup
+mkdir -p ${SCRIPTPATH}/repos
+gh repo clone magoel/vimsetup ${SCRIPTPATH}/repos/vimsetup
+rm -f ~/.vim 
+rm -f ~/.vimrc
+mkdir -p ~/vim-swap
+ln -s ${SCRIPTPATH}/repos/vimsetup/vimfiles ~/.vim
+mkdir -p ~/.vim/bundle
+
+#install vim plugins 
+gh repo clone VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+vim -E -s << EOF
+:PluginInstall
+:quit
+EOF
+
+
+
+sudo apt-get install build-essential -y
+sudo apt-get install gdb -y
+sudo apt-get install global -y
+sudo apt install exuberant-ctags -y
+sudo apt install vim-gtk -y    # needed for clipboard support
+sudo apt install cmake -y
+
+#sudo apt install vim-nox python3-dev -y
+#sudo apt install mono-complete golang default-jdk -y
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+
+
+# install node
+curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+nvm install --lts
